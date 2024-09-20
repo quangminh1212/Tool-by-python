d541f11689355156019dff75da0cf870d65e6971
Bach Minh Quang <72701749+quangminh1212@users.noreply.github.com>
2024-09-20 11:16:07 +0700

    Delete Tool AddKey  directory

--- Tool AddKey /Run.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Chạy script Python để chỉnh sửa firmware
-python3 modify_firmware.py input_firmware.hex output_firmware.hex
-
-# Nạp firmware đã chỉnh sửa vào thiết bị
-# Lệnh sau đây là ví dụ và sẽ thay đổi dựa trên công cụ nạp và thiết bị của bạn
-nrfjprog --program output_firmware.hex --sectorerase
-nrfjprog --reset
